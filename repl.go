eafccbf81863b7193824583779381c9691cf230e
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-08-28 21:26:06 +0300

    adding the cases

--- repl.go
+++ repl.go
@@ -16,7 +16,28 @@ func startRepl() {
 
 		text := scanner.Text()
 
-		fmt.Println("echoing: ", text)
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0{
+			continue
+		}
+
+		command := cleaned[0]
+
+		switch command {
+			case "help":
+				fmt.Println("Welcome to the Pokedex help menu")
+				fmt.Println("Here are your avilable commands")
+				fmt.Println(" - help")
+				fmt.Println(" - exit")
+				fmt.Println(" ")
+
+			case "exit":
+				os.Exit(0)
+		 default:
+			fmt.Println("Invalid command")
+		}
+
+		fmt.Println("echoing: ", cleaned)
 	}
 }
 
